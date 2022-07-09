4ee84eaf28a18165279cd31bfc556fd4cfd0da07
Vyshak-NSC <108945298+Vyshak-NSC@users.noreply.github.com>
2022-07-09 20:34:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Notes from "./components/Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is a heading.</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea
+        voluptates distinctio eaque minus nisi?
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
